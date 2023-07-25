5084e31026970503b10ed82bc636e0047fa9acab
Deanna Stevens <deanna.sofias@gmail.com>
2023-07-25 16:07:18 -0400

    Add querySelectors

---
+++ main.js
@@ -0,0 +1,30 @@
+//GOAL
+//When the button is clicked 5 random and new colors should appear
+//the color's hex codes should appear below each color box
+//the current palette should be tracked somewhere in your Data Model and updated when a new palette is generated
+
+// QUERY SELECTORS:
+
+var newPaletteBtn = document.querySelector(".newPaletteBtn");
+var box1Main = document.querySelector(".box1");
+var code1Main = document.querySelector(".code1");
+
+var box1Main = document.querySelector(".box2");
+var code1Main = document.querySelector(".code2");
+
+var box1Main = document.querySelector(".box3");
+var code1Main = document.querySelector(".code3");
+
+var box1Main = document.querySelector(".box4");
+var code1Main = document.querySelector(".code4");
+
+var box1Main = document.querySelector(".box5");
+var code1Main = document.querySelector(".code5");
+
+// EVENT LISTNER:
+
+newPaletteBtn.addEventListener("click", randomizeColors);
+
+//FUNCTIONS
+
+function randomizeColors() {}
